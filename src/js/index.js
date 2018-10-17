18206a8c6557dac595b8f871ca58c4c648a6b83b
Kevin Lindström <0007keli@res.ludvika.intra>
2018-10-17 11:37:50 +0200

    fixade ordningen på TODO

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+// TODO: ett chatt user interface
+// TODO: skriv ut meddelande
+// TODO: kunna välja ett användarnamn
+// TODO: skicka meddelande till webbsocket
+// TODO: innan meddelandet skickas ska det konverteras till json 
+// TODO: tar emot meddelande från webbsocket
+// TODO: konverterar från json till javascript
+// TODO: hämta in meddelande från input när användaren trycker på knappen
+// TODO: en enklare dokumentation
+
+
+
