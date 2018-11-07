8dc5331af4c4ac11c88fe2eac6703bb0ac681a6e
Kevin Lindström <0007keli@res.ludvika.intra>
2018-11-07 11:44:19 +0100

    onsdag

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,43 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
-// TODO: ett chatt user interface
 // TODO: skriv ut meddelande
 // TODO: kunna välja ett användarnamn
-// TODO: skicka meddelande till webbsocket
-// TODO: innan meddelandet skickas ska det konverteras till json 
 // TODO: tar emot meddelande från webbsocket
 // TODO: konverterar från json till javascript
-// TODO: hämta in meddelande från input när användaren trycker på knappen
 // TODO: en enklare dokumentation
+// TODO: ett chatt user interface
+const connection = new WebSocket('ws://104.248.143.87:1337');
 
+connection.onmessage = message => {
+  const textEl = document.getElementById('logger');
+  const obj = JSON.parse(message.data);
+  if (obj.type === 'heartbeat') {
+    textEl.textcontent += `${obj.data}, `;
+  }
+  console.log(obj.data.text);
+  const chattmessage = document.getElementById('chattmessage');
+  const div = document.getElementById('send');
+  const chattmessage = document.importNode(chattmessageDiv, true);
+  const clone = document.importNode(chattmesage.content, true);
+  clone.textcontent = obj.data.text;
+  const paste = document.getElementById('paste');
+  paste.appendChild(clone);
+};
 
+connection.onclose = () => {
+  console.log('uppkoppling nedstängd...');
+};
 
+const chatt = document.getElementById('chatt');
+chatt.addEventListener('click', () => {
+  const obj = {
+    type: 'textinput.value',
+    data: 'chatt meddelande som ska skickas',
+    key: 'password'
+  };
+  const jsonObj = JSON.stringify(obj);
+  connection.send(jsonObj);
+});
+const textinput = document.getElementById('inputtext');
+console.log(textinput.value);
